116c7770ee435849df332b62e70f005e639fac56
Stephen Maingi <StephenMaingi@KKSecurity.com>
2017-05-26 18:11:57 +0300

    New update

--- register.php
+++ register.php
@@ -1,42 +1,3 @@
-<?php require_once('connection/con.php'); ?>
-<?php
-if (!function_exists("GetSQLValueString")) {
-  function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "")
-  {
-    if (PHP_VERSION < 6) {
-      $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
-    }
-
-    $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
-
-    switch ($theType) {
-      case "text":
-        $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-        break;
-      case "long":
-      case "int":
-        $theValue = ($theValue != "") ? intval($theValue) : "NULL";
-        break;
-      case "double":
-        $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
-        break;
-      case "date":
-        $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-        break;
-      case "defined":
-        $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
-        break;
-    }
-    return $theValue;
-  }
-}
-
-mysql_select_db($database_conn, $con);
-$query_Recordset1 = "SELECT * FROM users";
-$Recordset1 = mysql_query($query_Recordset1, $con) or die(mysql_error());
-$row_Recordset1 = mysql_fetch_assoc($Recordset1);
-$totalRows_Recordset1 = mysql_num_rows($Recordset1);
-?>
 
 <!DOCTYPE html>
 <html>
@@ -192,9 +153,8 @@ $totalRows_Recordset1 = mysql_num_rows($Recordset1);
                 }
             }
         }
-      
-      ?>
 
+?>
 
    <!-- <div class="social-auth-links text-center">
       <p>- OR -</p>
@@ -273,6 +233,3 @@ $totalRows_Recordset1 = mysql_num_rows($Recordset1);
 
 </body>
 </html>
-<?php
-mysql_free_result($Recordset1);
-?>
